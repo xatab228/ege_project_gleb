77a2f6cbc710d5dd1818a6130161be191cfd2b9f
sctrg <sctrgleb@gmail.com>
2023-10-19 23:29:47 +0300

    #задание_5_cw_порешал задания

---
+++ Задание_5/18075.py
@@ -0,0 +1,10 @@
+a=[9999999]
+for i in range(100):
+    bin_n = bin(i)[2:]
+    for k in range(2):
+        count = bin_n.count('1')
+        bin_n= bin_n + str(count%2)
+    r=int(bin_n, 2)
+    if r>123:
+        a.append(r)
+print(min(a))
